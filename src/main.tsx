96403dfdac799ae42f0561046fdeebc605754860
MedoZzZ <mh639450@gmail.com>
2025-03-17 03:44:19 +0200

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientProvider>
 </React.StrictMode>
 );
