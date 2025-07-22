a122c93fe8819781379b989f5b297e8580a5a175
Mohamed2670 <mh639450@gmail.com>
2025-07-22 18:06:17 +0300

    search enhancment

--- src/old/Search.tsx
+++ src/old/Search.tsx
@@ -405,9 +405,13 @@ export const Search: React.FC = () => {
                                 {drug.name}
                               </button>
                             ))}
-
+                            {unmatched.length > 0 && (
+                              <div className="px-4 py-2 text-sm text-yellow-700 bg-yellow-50">
+                                Did you mean:{" "}
+                                                             </div>
+                            )}
                             {/* 🔵 Unmatched */}
-                            {unmatched.slice(1).map((drug, index) => (
+                            {unmatched.map((drug, index) => (
                               <button
                                 key={drug.id}
                                 id={`suggestion-unmatched-${index}`}
@@ -424,23 +428,6 @@ export const Search: React.FC = () => {
                               </button>
                             ))}
                             {/* 🟡 Did you mean */}
-                            {unmatched.length > 0 && (
-                              <div className="px-4 py-2 text-sm text-yellow-700 bg-yellow-50">
-                                Did you mean:{" "}
-                                <button
-                                  type="button"
-                                  onClick={() => {
-                                    setSearchQuery(unmatched[0].name);
-                                    setShowSuggestions(true);
-                                    handleDrugSelect(unmatched[0]);
-                                  }}
-                                  className="font-semibold text-blue-700 underline hover:text-blue-900"
-                                >
-                                  {unmatched[0].name}
-                                </button>
-                                ?
-                              </div>
-                            )}
                           </>
                         );
                       })()}
--- src/old/Search2.tsx
+++ src/old/Search2.tsx
@@ -746,11 +746,14 @@ export const InsuranceSearch: React.FC = () => {
                                   {drug.name}
                                 </button>
                               ))}
-
-                        
-
+                              {/* 🟡 Did you mean */}
+                              {unmatched.length > 0 && (
+                                <div className="px-4 py-2 text-sm text-yellow-700 bg-yellow-50">
+                                  Did you mean: 
+                                </div>
+                              )}
                               {/* 🔵 Remaining Unmatched */}
-                              {unmatched.slice(1).map((drug, index) => (
+                              {unmatched.map((drug, index) => (
                                 <button
                                   key={drug.id}
                                   id={`suggestion-unmatched-${index}`}
@@ -768,23 +771,6 @@ export const InsuranceSearch: React.FC = () => {
                                   {drug.name}
                                 </button>
                               ))}
-                                    {/* 🟡 Did you mean */}
-                              {unmatched.length > 0 && (
-                                <div className="px-4 py-2 text-sm text-yellow-700 bg-yellow-50">
-                                  Did you mean:{" "}
-                                  <button
-                                    type="button"
-                                    onClick={() => {
-                                      setDrugSearchQuery(unmatched[0].name);
-                                      handleDrugSelect(unmatched[0]);
-                                    }}
-                                    className="font-semibold text-blue-700 underline hover:text-blue-900"
-                                  >
-                                    {unmatched[0].name}
-                                  </button>
-                                  ?
-                                </div>
-                              )}
                             </>
                           );
                         })()}
